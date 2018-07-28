0234fe6141cd15638c2fa0c286a1b56a6f4b994d
Adam Smakulski <adam.smakulski@gmail.com>
2018-07-28 13:23:06 +0200

    c

---
+++ src/test/java/com/infoshareacademy/AppIT.java
@@ -0,0 +1,20 @@
+package com.infoshareacademy;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppIT
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( false );
+    }
+}
